acb51e8aceebe5a3d94127d69c0156584c01bfd0
stavrovskymax <stavrovsky96@yandex.ru>
2018-09-08 15:55:08 +0300

    update 08.09.2018

--- IQDemoException/ICharQ.java
+++
@@ -1,7 +0,0 @@
-public interface ICharQ {
-    // Put a character in the queue
-    void put(char ch) throws QueueFullException;
-
-    // Get a character from the queue
-    char get() throws QueueEmtyException;
-}
