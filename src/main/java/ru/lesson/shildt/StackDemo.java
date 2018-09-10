38e12fa75503f2982d993e8ca24e185f859028cd
stavrovskymax <stavrovsky96@yandex.ru>
2018-09-11 00:42:40 +0300

    Stack queue

--- src/main/java/ru/lesson/shildt/StackDemo.java
+++ src/main/java/ru/lesson/shildt/StackDemo.java
@@ -1,5 +1,26 @@
 package ru.lesson.shildt;
 
+class StackFullException extends Exception {
+ int size;
+
+ public StackFullException(int size) {
+  this.size = size;
+ }
+
+ @Override
+ public String toString() {
+  return "The stack is full. Max size is " + size;
+ }
+}
+
+class StackEmptyException extends Exception {
+
+ @Override
+ public String toString() {
+  return "The stack is empty.";
+ }
+}
+
 class Stack {
  private char stck[];
  private int putloc;
@@ -8,40 +29,40 @@ class Stack {
   stck = new char[size];
   putloc = 0;
  }
-    
+
  Stack(char a[]) {
   putloc = 0;
   stck = new char[a.length];
-  
+
   for(int i=0; i < a.length; i++)
-   pop(a[i]);      
+   try {
+    push(a[i]);
+   }
+
+   catch (StackFullException exc) {
+   System.out.println(exc);
+   }
  }
- 
+
  Stack(Stack ob) {
   putloc = ob.putloc;
   stck = new char[ob.stck.length];
-  
+
   for(int i=0; i < ob.stck.length; i++)
-   stck[i] = ob.stck[i];      
- } 
-     
- void pop(char ch) {
+   stck[i] = ob.stck[i];
+ }
+
+ void push(char ch) throws StackFullException {
   if (putloc==stck.length) {
-   System.out.println(" - Стек пуст");
-   return;
+   throw new StackFullException(stck.length);
   }
   stck[putloc] = ch;
   putloc++;
  }
- 
- int getPutloc() {
-  return putloc;
- }
 
- char push() {
+ char pop() throws StackEmptyException {
   if (putloc == 0) {
-   System.out.println(" - Стек заполнен");
-   return (char) 0;
+   throw new StackEmptyException();
   }
   putloc--;
   return stck[putloc];
@@ -49,45 +70,49 @@ class Stack {
 }
 class StackDemo {
  public static void main(String args[]) {
- 
-  Stack q1 = new Stack(10);
-  
-  char name[] = {'T', 'o', 'm'};
-  Stack q2 = new Stack(name);
-  
-  char ch;
-  int i;
 
-  System.out.print("Стек q1: ");
-  
-  for(i=0; i < 10; i++) {
-   q1.pop((char) ('A' + i));
-   System.out.print(q1.getPutloc() + " ");
-  }
-  
-  Stack q3 = new Stack(q1);
-  System.out.println("\n");
+  try {
+   Stack q1 = new Stack(10);
 
-  System.out.print("Содержимое  q1: ");
-  for(i=0; i < 10; i++) {
-   ch = q1.push();
-   if (ch != (char) 0) System.out.print(ch);
+   char name[] = {'T', 'o', 'm'};
+   Stack q2 = new Stack(name);
+
+   char ch;
+   int i;
+
+   System.out.print("Стек q1: ");
+
+   for (i = 0; i < 10; i++) {
+    q1.push((char) ('A' + i));
+   }
+
+   Stack q3 = new Stack(5);
+   System.out.println("\n");
+
+   System.out.print("Содержимое  q1: ");
+   for (i = 0; i < 10; i++) {
+    ch = q1.pop();
+    if (ch != (char) 0) System.out.print(ch);
+   }
+
+   System.out.println("\n");
+
+   System.out.print("Содрежимое q2: ");
+   for (i = 0; i < 3; i++) {
+    ch = q2.pop();
+    if (ch != (char) 0) System.out.print(ch);
+   }
+
+   System.out.println("\n");
+
+   System.out.print("Содержимое q3: ");
+   for (i = 0; i < 10; i++) {
+    ch = q3.pop();
+    if (ch != (char) 0) System.out.print(ch);
+   }
   }
-               
-  System.out.println("\n");
-  
-  System.out.print("Содрежимое q2: ");
-  for(i=0; i < 3; i++) {
-   ch = q2.push();
-   if (ch != (char) 0) System.out.print(ch);
+  catch (StackFullException | StackEmptyException exc) {
+   System.out.println(exc);
   }
-  
-  System.out.println("\n");
-  
-  System.out.print("Содержимое q3: ");
-  for(i=0; i < 10; i++) {
-   ch = q3.push();
-   if (ch != (char) 0) System.out.print(ch);
   }
  }
-}	
